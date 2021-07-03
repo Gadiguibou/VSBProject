e6227fbf550e53a54c59b260b697ae9eb9e55ffa
abubakar.daud@mail.mcgill.ca <IHat3YouM8>
2021-07-03 15:54:08 -0400

    blue

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/blue', views.blue, name='blue'),
+    path('blue', views.blue, name='blue'),
     path('', views.index, name='index'),
     path('JSON', views.json, name='json'),
     path('date', views.date_test, name='date')
