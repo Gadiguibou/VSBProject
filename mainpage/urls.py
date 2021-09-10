df509f645476db57230c3ff3e08c5d16181eaaf2
abubakar.daud@mail.mcgill.ca <abubakar.daud@mail.mcgill.ca>
2021-09-10 14:40:23 -0400

    remove

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('blue', views.blue, name='blue'),
     path('class/<int:term>/<slug:class_name>', views.classfinder, name="classfinder"),
     path('', views.index, name='index'),
-    path('account', views.success, name="account"),
-    path('API', views.success, name="API"),
-    path('contact', views.success, name="contact"),
+    path('account', views.account, name="account"),
+    path('API', views.API, name="API"),
+    path('contact', views.contact, name="contact"),
 
 ]
